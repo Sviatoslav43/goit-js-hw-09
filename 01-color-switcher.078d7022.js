d4e701c2dd72779ae946cb61aa910008b30f55e8
Sviatoslav43 <106432553+Sviatoslav43@users.noreply.github.com>
2022-10-27 08:31:26 +0000

    Deploying to gh-pages from @ Sviatoslav43/goit-js-hw-09@ef75eba2d403118e64fa59af09cdfabb16ccdc36 🚀

---
+++ 01-color-switcher.078d7022.js
@@ -0,0 +1,2 @@
+!function(){var t={start:document.querySelector("button[data-start]"),stop:document.querySelector("button[data-stop]")},e=null;t.start.addEventListener("click",(function(){e=setInterval((function(){document.querySelector("body").style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),t.stop.addEventListener("click",(function(){clearInterval(e)}))}();
+//# sourceMappingURL=01-color-switcher.078d7022.js.map
---
+++ 01-color-switcher.2847c656.js
@@ -0,0 +1,2 @@
+const t={start:document.querySelector("button[data-start]"),stop:document.querySelector("button[data-stop]")};let e=null;t.start.addEventListener("click",(()=>{e=setInterval((()=>{document.querySelector("body").style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),t.stop.addEventListener("click",(()=>{clearInterval(e)}));
+//# sourceMappingURL=01-color-switcher.2847c656.js.map
