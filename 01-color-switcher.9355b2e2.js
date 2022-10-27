3af608623dfeb70ff55fa97dd06d6c67472d88d1
Sviatoslav43 <106432553+Sviatoslav43@users.noreply.github.com>
2022-10-27 08:41:40 +0000

    Deploying to gh-pages from @ Sviatoslav43/goit-js-hw-09@bb050d76942f8c7b7450c84c8956129aebba1472 🚀

---
+++ 01-color-switcher.9355b2e2.js
@@ -0,0 +1,2 @@
+!function(){var t={start:document.querySelector("button[data-start]"),stop:document.querySelector("button[data-stop]")},e=null;t.start.addEventListener("click",(function(){e=setInterval((function(){document.querySelector("body").style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16)),t.start.disabled=!0}),1e3)})),t.stop.addEventListener("click",(function(){clearInterval(e),t.start.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.9355b2e2.js.map
