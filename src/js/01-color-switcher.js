ef75eba2d403118e64fa59af09cdfabb16ccdc36
Sviatoslav43 <navolskiy17@gmail.com>
2022-10-27 11:30:44 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const refs = {
+start: document.querySelector('button[data-start]'),
+stop: document.querySelector('button[data-stop]'),
+};
 
+let timeId = null
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+refs.start.addEventListener('click', () => {
+    timeId = setInterval(() => {
+        const body = document.querySelector('body')
+        body.style.backgroundColor = getRandomHexColor()
+        
+    },1000)
+})
+
+
+
+refs.stop.addEventListener('click', () => {
+    clearInterval(timeId)
+})
