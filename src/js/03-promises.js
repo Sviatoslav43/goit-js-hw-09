e9aedf9b64166d109a40090e95d46107f13524eb
Sviatoslav43 <navolskiy17@gmail.com>
2022-11-01 18:10:14 +0200

    ч

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -28,9 +28,10 @@ function promiseHandler(e) {
 }
 
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-
+  
   return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    
     setTimeout(() => {
       if (shouldResolve) {
         resolve({ position, delay });
